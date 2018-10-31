66225fa4d2090ed0e0479619e19f154c31b222a9
Malte Weiß <malteweiss@yahoo.de>
2018-10-31 14:26:33 +0100

    Implemented demo commands

--- Assets/GameControllerCommands.cs
+++ Assets/GameControllerCommands.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [ExecutableFromConsole]
 public class GameControllerCommands : MonoBehaviour {
 
+    public GameObject cube;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -15,9 +17,23 @@ public class GameControllerCommands : MonoBehaviour {
 		
 	}
 
-    [ConsoleMethod("do_something", "Does something")]
-    public void DoSomething()
+    [ConsoleMethod("cube_rotate", "Rotates the cube by to the given angle.")]
+    public void RotateCubeBy(float degrees)
     {
-        Debug.Log("yay");
+        cube.transform.Rotate(Vector3.up, degrees, Space.World);
+    }
+
+    [ConsoleMethod("cube_scale", "Scales the cube by the given factor.")]
+    public void ScaleCubeBy(float factor)
+    {
+        cube.transform.localScale = Vector3.one * factor;
+    }
+
+    [ConsoleMethod("cube_random_color", "Randomizes the color of the cube.")]
+    public void RandomizeCubeColor()
+    {
+        var cubeRenderer = cube.GetComponent<MeshRenderer>();
+        var mat = cubeRenderer.material;
+        mat.SetColor("_Color", new Color(Random.value, Random.value, Random.value));
     }
 }
